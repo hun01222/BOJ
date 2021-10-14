214737d5c25d56526acd394ebe7a27a87adf26bf
hun01222 <hun0122232@gmail.com>
2021-10-14 09:22:53 +0900

    new

--- 1644.cpp
+++ 1644.cpp
@@ -10,23 +10,31 @@ int main(){
     int k=0;
     for(int i=2; i<=sqrt(max); i++){
         if(arr[i]==0){
-            prime[k++]=i;
             for(int j=i*2; j<=max; j+=i)
                 arr[j]=1;
         }
     }
 
+    for(int i=0; i<=max; i++){
+        if(arr[i]==0)
+            prime[k++]=i;
+    }
+
     int n, start=0, end=0, temp=0, cnt=0;
     cin >> n;
-    while(end<=k){
-        if(temp<n){
-            temp+=prime[end++];
+    while(1){
+        if(temp>=n){
+            temp-=prime[++start];
         }
-        else if(temp>=n){
-            temp-=prime[start++];
+        else if(end==k-1)
+            break;
+        else if(temp<n){
+            temp+=prime[++end];
         }
+
         if(temp==n){
             cnt++;
+            //cout << prime[start-1] << prime[end-1] << "\n";
         }
     }
     cout << cnt;
