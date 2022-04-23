382ba8f914acc4a4ca558cd00c1c877a6bb5c7a5
박지훈 <hun01222@naver.com>
2022-04-23 14:53:06 +0000

    add

---
+++ PS/16139.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	string s;
+	int n;
+	cin >> s >> n;
+	
+	int alphabet, start, end;
+	
+	
+	
+}
