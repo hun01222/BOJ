ae152b295509365f4f31de930a342efb7df148dc
박지훈 <hun01222@naver.com>
2022-04-24 06:27:26 +0000

    add

--- PS/16139.cpp
+++ PS/16139.cpp
@@ -1,17 +1,46 @@
 #include<iostream>
 #include<string>
-#include<map>
-#include<vector>
 #include<algorithm>
 using namespace std;
 
+// end -> start
 int main(){
 	string s;
-	int n;
+	int n, temp_start=0, temp_end=0;
+	int arr[26][3]={0, };
 	cin >> s >> n;
+	arr[s[0]-97][0]++;
 	
-	int alphabet, start, end;
-	
-	
-	
+	while(n--){
+		int start, end;
+		char alpha;
+		cin >> alpha >> end >> start;
+		
+		temp_start=arr[alpha-97][1];
+		temp_end=arr[alpha-97][2];
+		arr[alpha-97][1]=start;
+		arr[alpha-97][2]=end;
+		
+		if(temp_start<start){
+			for(int i=temp_start+1; i<=start; i++){
+				arr[s[i]-97][0]++;
+			}
+		}
+		if(temp_end>end){
+			for(int i=temp_end-1; i>=end; i--){
+				arr[s[i]-97][0]++;
+			}
+		}
+		if(temp_start>start){
+			for(int i=temp_start; i>start; i--){
+				arr[s[i]-97][0]--;
+			}
+		}
+		if(temp_end<end){
+			for(int i=temp_end; i<end; i++){
+				arr[s[i]-97][0]--;
+			}
+		}
+		cout << arr[alpha-97][0] << "\n";
+	}
 }
