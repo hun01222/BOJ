0b582bb29b586e852cf0cceddca17cad30068a75
박지훈 <hun01222@naver.com>
2022-04-25 14:51:45 +0000

    new

---
+++ PS/4358.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	string s;
+	map<string, float> map;
+	int cnt=0;
+	while(getline(cin, s)){
+		cnt++;
+	}
+}
