60289d8d91d7623bbd9c5b153903cd0a896c22b5
박지훈 <hun01222@naver.com>
2022-04-28 14:57:28 +0000

    new

---
+++ PS/14503.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n, m, r, c, d, cnt=0;
+	int map[51][51]={0, };
+	cin >> n >> m >> r >> c >> d;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			cin >> map[i][j];
+		}
+	}
+	int temp_x=r, temp_y=c;
+	arr[r][c]=2;
+	cnt++;
+	while(){
+		if(d==0){
+			
+		}
+		else if(d==1){
+			
+		}
+		else if(d==2){
+			
+		}
+		else if(d==3){
+			
+		}
+	}
+	cout << cnt;
+}
+
+/*
+1 1 1 1 1 1 1 1 1 1
+1 0 0 0 0 0 0 0 0 1
+1 0 0 0 1 1 1 1 0 1
+1 0 0 1 1 2 0 0 0 1
+1 0 1 1 2 2 0 0 0 1
+1 0 0 0 2 0 0 0 0 1
+1 0 0 0 2 0 0 1 0 1
+1 0 0 0 2 0 1 1 0 1
+1 0 0 0 0 0 1 1 0 1
+1 0 0 0 0 0 0 0 0 1
+1 1 1 1 1 1 1 1 1 1
+*/
