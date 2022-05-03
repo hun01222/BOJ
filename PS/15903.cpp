c1a35ebae5da943b9a324f0c7885b8ce1f8fc196
박지훈 <hun01222@naver.com>
2022-05-03 13:23:48 +0000

    new

--- PS/15903.cpp
+++ PS/15903.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 #include<queue>
+typedef long long ll;
 using namespace std;
 
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	
-	int n, m, ans=0;
-	priority_queue<int> pq;
+	ll n, m, ans=0;
+	priority_queue<ll> pq;
 	cin >> n >> m;
 	for(int i=0; i<n; i++){
-		int temp;
+		ll temp;
 		cin >> temp;
 		pq.push(-temp);
 	}
 	
 	while(m--){
-		int temp1=pq.top();
+		ll temp1=pq.top();
 		pq.pop();
-		int temp2=pq.top();
+		ll temp2=pq.top();
 		pq.pop();
 		pq.push(temp1+temp2);
 		pq.push(temp1+temp2);
