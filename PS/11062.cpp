90ff492840e973c0ec7835b23324f396d2da8acf
박지훈 <hun01222@naver.com>
2022-05-05 14:51:27 +0000

    new

--- PS/11062.cpp
+++ PS/11062.cpp
@@ -1,24 +1,37 @@
 #include<iostream>
-#include<queue>
+#include<algorithm>
+#include<memory.h>
 using namespace std;
 
-int main(){
-	int t, n;
-	deque<int> dq;
-	cin >> t;
+int dp[1010][1010][2];
+int cost[1010];
+
+int solve(int flag, int i, int j){
+	int &ret=dp[i][j][flag];
+	if(ret!=-1)
+		return ret;
+	if(i>=j){
+		if(!flag)
+			return ret=cost[i];
+		else
+			return ret=0;
+	}
 	
+	if(!flag)
+		return ret=max(solve(i+1, j, !flag)+cost[i], solve(i, j-1, !flag)+cost[j]);
+	else
+		return ret=min(solve(i+1, j, !flag), solve(i, j-1, !flag));
+}
+
+int main(){
+	int t;
+	cin >> t;
 	while(t--){
+		memset(dp, -1, sizeof(dp));
+		int n;
 		cin >> n;
-		int ans=0, temp;
-		for(int i=0; i<n; i++){
-			cin >> temp;
-			dq.push_front(temp);
-		}
-		whlie(dp.size()>1){
-			
-		}
-		
-		ans+=dp.front();
-		cout << ans << "\n";
+		for(int i=1; i<=n; i++)
+			cin >> cost[i];
+		cout << solve(1, n, 0) << "\n";
 	}
 }
