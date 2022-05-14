0ec5eb71e3120b0a887b11953a407a1797275a18
박지훈 <hun01222@naver.com>
2022-05-14 14:59:49 +0000

    new

---
+++ PS/24444.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#define max 100010
+using namespace std;
+
+vector<int> graph[max];
+queue<int> q;
+int ans[max]={0, };
+int cnt=1;
+
+void BFS(){
+	while(!q.empty()){
+		int start=q.front();
+		q.pop();
+		ans[start]=cnt++;
+		for(int i=0; i<graph[start].size(); i++){
+			if(ans[graph[start][i]]!=0)
+				continue;
+			q.push(graph[start][i]);
+			ans[graph[start][i]]=1;
+		}
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int V, E, start;
+	cin >> V >> E >> start;
+	for(int i=0; i<E; i++){
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for(int i=1; i<=V; i++)
+		sort(graph[i].begin(), graph[i].end());
+	
+	q.push(start);
+	BFS();
+	
+	for(int i=1; i<=V; i++)
+		cout << ans[i] << "\n";
+}
---
+++ PS/24480.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#define max 100010
+using namespace std;
+
+vector<int> graph[max];
+int ans[max]={0, };
+int cnt=1;
+
+void DFS(int start){
+	for(int i=0; i<graph[start].size(); i++){
+		if(ans[graph[start][i]]==0){
+			ans[graph[start][i]]=(cnt++);
+			DFS(graph[start][i]);
+		}
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int V, E, start;
+	cin >> V >> E >> start;
+	for(int i=0; i<E; i++){
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for(int i=1; i<=V; i++)
+		sort(graph[i].begin(), graph[i].end(), greater<>());
+	
+	ans[start]=cnt++;
+	DFS(start);
+	
+	for(int i=1; i<=V; i++)
+		cout << ans[i] << "\n";
+}
