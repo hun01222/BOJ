9892baaf468946d678d32041fd4707036e134169
박지훈 <hun01222@naver.com>
2022-05-20 01:20:00 +0000

    new

--- PS/1963.cpp
+++ PS/1963.cpp
@@ -17,19 +17,49 @@ int main(){
 	int t;
 	cin >> t;
 	while(t--){
-		int a, b;
+		int a, b, mem[10001]={0, };
 		queue<pair<int, int>> q;
 		cin >> a >> b;
 		
 		q.push(make_pair(a, 0));
 		while(!q.empty()){
-			if(q.front().second==b){
-				cout << cnt;
-				return 0;
-			}
-			int temp=q.front().first;
+			int num=q.front().first;
+			int cnt=q.front().second;
 			q.pop();
+			if(mem[num])
+				continue;
+			mem[num]=1;
+			if(num==b){
+				cout << cnt << "\n";
+				break;
+			}
 			
+			for(int i=1; i<10; i++){
+				int temp=num;
+				temp%=1000;
+				if(v[temp+1000*i])
+					q.push(make_pair(temp+1000*i, cnt+1));
+			}
+			for(int i=0; i<10; i++){
+				int temp=num;
+				int share=(temp/1000);
+				temp%=100;
+				if(v[share*1000+100*i+temp])
+					q.push(make_pair(share*1000+100*i+temp, cnt+1));
+			}
+			for(int i=0; i<10; i++){
+				int temp=num;
+				int share=(temp/100);
+				temp%=10;
+				if(v[share*100+10*i+temp])
+					q.push(make_pair(share*100+10*i+temp, cnt+1));
+			}
+			for(int i=0; i<10; i++){
+				int temp=num;
+				int share=(temp/10);
+				if(v[share*10+i])
+					q.push(make_pair(share*10+i, cnt+1));
+			}
 		}
 	}
 }
