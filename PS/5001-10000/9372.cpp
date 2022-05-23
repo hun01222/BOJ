a33097ccaed11c9df760ef590e815a8ab512c91e
박지훈 <hun01222@naver.com>
2022-05-23 14:56:16 +0000

    new

---
+++ PS/5001-10000/9372.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n, m;
+		int tree[1001]={0, };
+		cin >> n >> m;
+		for(int i=1; i<=n; i++)
+			tree[i]=i;
+		for(int i=0; i<m; i++){
+			
+		}
+	}
+}
