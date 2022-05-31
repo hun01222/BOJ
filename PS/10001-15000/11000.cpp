2dad9532c9604760b61343a668d3034be8996081
박지훈 <hun01222@naver.com>
2022-05-31 14:23:18 +0000

    new

---
+++ PS/10001-15000/11000.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+	int n;
+	vector<pair<int, int>> v;
+	
+	cin >> n;
+	for(int i=0; i<n; i++){
+		int a, b;
+		cin >> a >> b;
+		v.push_back(make_pair(a, b));
+	}
+}
