40f41a1fb219e50192ad1b72a3e61ad57a5e7361
박지훈 <hun01222@naver.com>
2022-05-31 14:41:52 +0000

    new

--- PS/10001-15000/11000.cpp
+++ PS/10001-15000/11000.cpp
@@ -1,15 +1,29 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
+struct time{
+	int start, stop;
+	bool check;
+};
+
+bool cmp(time a, time b){
+	if(a.stop==b.stop)
+		return a.start<b.start;
+	return a.stop<b.stop;
+}
 
 int main(){
 	int n;
-	vector<pair<int, int>> v;
+	vector<time> v;
 	
 	cin >> n;
 	for(int i=0; i<n; i++){
 		int a, b;
 		cin >> a >> b;
-		v.push_back(make_pair(a, b));
+		v.push_back({a, b, false});
 	}
+	sort(v.begin(), v.end(), cmp);
+	
 }
