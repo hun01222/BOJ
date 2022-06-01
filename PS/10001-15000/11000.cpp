46c5a09b7349669a30de57af75ca2d6187052d45
박지훈 <hun01222@naver.com>
2022-06-01 14:03:23 +0000

    new

--- PS/10001-15000/11000.cpp
+++ PS/10001-15000/11000.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<algorithm>
 using namespace std;
 
-struct time{
-	int start, stop;
-	bool check;
-};
-
-bool cmp(time a, time b){
-	if(a.stop==b.stop)
-		return a.start<b.start;
-	return a.stop<b.stop;
-}
+vector<pair<int, int>> v;
+priority_queue<int> pq;
 
 int main(){
-	int n;
-	vector<time> v;
-	
+	int n;	
 	cin >> n;
+	
 	for(int i=0; i<n; i++){
 		int a, b;
 		cin >> a >> b;
-		v.push_back({a, b, false});
+		v.push_back(make_pair(a, b));
 	}
-	sort(v.begin(), v.end(), cmp);
+	sort(v.begin(), v.end());
 	
+	pq.push(-v[0].second);
+	for(int i=1; i<n; i++){
+		pq.push(-v[i].second);
+		if(-pq.top()<=v[i].first)
+			pq.pop();
+	}
+	
+	cout << pq.size();
 }
