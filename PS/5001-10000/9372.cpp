9020a12df191c5d90b4e5ad29ab755082d52b398
박지훈 <hun01222@naver.com>
2022-06-07 14:08:38 +0000

    new

--- PS/5001-10000/9372.cpp
+++ PS/5001-10000/9372.cpp
@@ -5,13 +5,13 @@ int main(){
 	int t;
 	cin >> t;
 	while(t--){
-		int n, m;
-		int tree[1001]={0, };
+		int n, m, temp1, temp2;
 		cin >> n >> m;
-		for(int i=1; i<=n; i++)
-			tree[i]=i;
-		for(int i=0; i<m; i++){
-			
-		}
+		for(int i=0; i<m; i++)
+			cin >> temp1 >> temp2;
+		
+		cout << n-1 << "\n";
 	}
 }
+
+// 비행기를 타는 횟수는 중요하지 않다!!
