5d04b8db01b4d72096f34b88b29b15406343a12d
박지훈 <hun01222@naver.com>
2023-02-27 14:53:58 +0000

    new

---
+++ PS/5001-10000/9001-10000/9086.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int n;
+	string s;
+	cin >> n;
+	
+	while(n--){
+		cin >> s;
+		cout << s[0] << s[s.size()-1] << "\n";
+	}
+}
